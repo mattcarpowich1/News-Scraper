5c35422655795c9f62f7cec7a2714552e7465d6b
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2017-11-23 13:24:04 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const index = require('./controllers/index.js');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+app.listen(PORT);
